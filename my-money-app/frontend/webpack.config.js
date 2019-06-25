4945391a8ae221ee1a7ca17f79715621ab2c8da0
Roger Silva <roger.silva@prestadores.amil.com.br>
2019-06-25 17:17:45 -0300

    Atualizando repositorio aula 117 - secao 9

--- my-money-app/frontend/webpack.config.js
+++ my-money-app/frontend/webpack.config.js
@@ -1,47 +1,47 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require ('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
-    output:{
+    output: {
         path: __dirname + '/public',
-        filename:'./app.js'
+        filename: './app.js'
     },
-    devServer:{
-        port:8080,
+    devServer: {
+        port: 8080,
         contentBase: './public',
     },
-    resolve:{
-        extensions:['', '.js', '.jsx'],
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
-            jquery:'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins:[
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
-            'windows.jQuery': 'jquery'
+            'window.jQuery': 'jquery'
         }),
         new ExtractTextPlugin('app.css')
     ],
-    module:{
-        loaders:[
-            {
-                test:/.js[x]?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query:{
-                    presets : ['es2015', 'react'],
-                    plugins:['transform-object-rest-spread']
-                }
-            },
-            {
-                test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
-                loader: 'file'
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
             }
-        ]
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
+        }]
     }
 }
