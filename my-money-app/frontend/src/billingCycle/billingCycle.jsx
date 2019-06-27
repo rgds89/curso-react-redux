dd75e98dd49f63db3ad4672bcd6e6668952ad484
Roger Silva <roger.silva@prestadores.amil.com.br>
2019-06-27 10:25:44 -0300

    Atualizando repositorio aula 119 - secao 9

---
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>BillingCycle</h1>
+    </div>
+    
+)
--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import '../common/template/dependencies'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
+import Routes from './routes/routes'
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <SideBar />
+        <div className='content-wrapper'>
+            <Routes />
+        </div>
+        <Footer />
     </div>
 )
