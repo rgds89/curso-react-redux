f6859ec4a7eef7256124faf0f28a9216ac9766a6
Roger Silva <roger.silva@prestadores.amil.com.br>
2019-07-04 15:18:58 -0300

    Atualizando repositorio aula 126 secao 9

--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
+
 
 class Dashboard extends Component{
     render (){
@@ -9,12 +13,21 @@ class Dashboard extends Component{
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de Créditos' />
-                    <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10' text='Total de Débitos' />
-                    <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Valor Consolidado' />
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10' text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Valor Consolidado' />
+                    </Row>                    
                 </Content>
             </div>
         )
     }
 }
-export default Dashboard
+
+const mapStateToProps = state => (
+    {
+        summary: state.dashboard.summary,
+    }
+)
+
+export default connect (mapStateToProps) (Dashboard)
