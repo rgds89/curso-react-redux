b89860f5d293971bd5ef20abf41ebdcf40fa1385
Roger Silva <roger.silva@prestadores.amil.com.br>
2019-07-10 14:08:04 -0300

    Atualizando repositorio

--- my-money-app/frontend/src/dashboard/dashBoardReducer.js
+++ my-money-app/frontend/src/dashboard/dashBoardReducer.js
@@ -1,15 +1,17 @@
 const INITIAL_STATE = {
     summary:{
-        credit: 0,
-        debit: 0
+        credit:0,
+        debit:0
     }
 }
 
-export default function (state = INITIAL_STATE, action){
+
+export default function(state = INITIAL_STATE, action){
     switch(action.type){
         case 'BILLING_SUMMARY_FETCHED':
             return {...state, summary: action.payload.data}
         default:
             return state
+
     }
 }
