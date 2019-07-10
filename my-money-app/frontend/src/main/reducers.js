55936cb5fcd9852018f96fbe6a9035900b180fee
Roger Silva <roger.silva@prestadores.amil.com.br>
2019-07-10 14:23:59 -0300

    Corrigindo arquivos

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,21 +1,11 @@
+    
 import { combineReducers } from 'redux'
 
-<<<<<<< HEAD
-import DashBoardReducer from '../dashboard/dashBoardReducer'
+
+import DashboardReducer from '../dashboard/dashboardReducer'
 
 const rootReducer = combineReducers({
-    dashboard: DashBoardReducer
-=======
-//Reducers
-import dashboardReducer from '../dashboard/dashboardReducer'
-
-const rootReducer = combineReducers({
-    dashboard: () => (
-        {
-            summary: dashboardReducer
-        }
-    )
->>>>>>> 638f9782fc807b19919ee491a52e0dd868d39e2c
+    dashboard: DashboardReducer,
 })
 
 export default rootReducer
