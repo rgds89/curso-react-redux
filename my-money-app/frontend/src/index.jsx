180db13404c4a0b708bcfa4669f9e70b50b26f92
Roger Silva <roger.silva@matera.com>
2019-09-16 11:22:19 -0300

    Atualizando repositorio aula 132 secao 132

--- my-money-app/frontend/src/index.jsx
+++ my-money-app/frontend/src/index.jsx
@@ -7,8 +7,8 @@ import promise from 'redux-promise'
 import App from './main/app'
 import Reducers from './main/reducers'
 
-
-const store = applyMiddleware(promise)(createStore)(Reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(Reducers, devTools)
 ReactDom.render(
     <Provider store={store}>
         <App />
